bc7b9d00a63384464295f8d7851a06c4f69e24e6
atahualpaxd <victortadeo207@gmail.com>
2024-10-27 00:00:53 -0500

    cambios juegos

--- GameJamEquisDe/GameJamEquisDe.cpp
+++ GameJamEquisDe/GameJamEquisDe.cpp
@@ -1,16 +1,6 @@
 #include "pch.h";
-#include <iostream>
-#include <Windows.h>
-using namespace System;
-using namespace std;
+#include "MisFunciones.h"
 
-const int HEIGHT = 120;
-const int WIDTH = 40;
-
-void configWindow() {
-    Console::SetWindowSize(HEIGHT, WIDTH);
-    Console::CursorVisible = false;
-}
 void showIntro() {
 
 
@@ -21,8 +11,6 @@ void showIntro() {
     cout << "\t\t\t\t |____/|_|Y___|_| |_|Y_/ L___|_| |_|_|L__,_|L___/  ";
 
 
-
-
     cout << "\n\n\n\n\n\n\n\n\n\n\t\t\t\t ";
     char x = 219;
     for (int i = 0; i <= 40; i++) {
@@ -30,7 +18,6 @@ void showIntro() {
         cout << x;
     }
 }
-
 int mostrarmenu() {
     int elegir;
 
@@ -47,10 +34,76 @@ int mostrarmenu() {
     return elegir;
 }
 
+void IniciarCarrera() {
+    int cuenta_carreras = 1;
+    bool hay_ganador = false;
+    int carreras_c1 = 0, carreras_c2 = 0;
+    const int Meta = 150;
+         titulo(28, 1);     meta(Meta, 3);
+    //coordenadas de los caracteres
+    float x1, y1, dx1;
+    float x2, y2, dx2;
+
+    x1 = x2 = 1;
+    y1 = 10;
+    y2 = 20;
+    dx1 = aleatorio_decimal();
+    dx2 = aleatorio_decimal();
+
+    cursor(x1, y1); cout << "@";
+    cursor(x2, y2); cout << "O";
+
+    while (1) {
+
+        panel_control(dx1, dx2, x1, x2, carreras_c1, carreras_c2);
+
+        cursor(35, 5); cout << "CARRERA " << cuenta_carreras;
+        //borrar
+        borra_auto(x1, y1);
+
+        borraauto2(x2, y2);
+        //mover
+        x1 += dx1;
+        x2 += dx2;
+        //dibujar
+        dibuja_auto(x1, y1);
+
+        dibujarauto2(x2, y2);
+
+        //verifica quien gana 
+        if (x1 > Meta - 9) {
+            cursor(30, 35); cout << "GANA CARACTER 1";
+            system("pause>0");
+            hay_ganador = true;
+            carreras_c1++;
+        }
+
+        if (x2 > Meta -8) {
+            cursor(30, 35); cout << "GANA CARACTER 2";
+            system("pause>0");
+            hay_ganador = true;
+            carreras_c2++;
+        }
+
+        if (hay_ganador) {
+            //reinicia una nueva carrera
+            Console::Clear();
+            titulo(28, 1);     meta(Meta, 3);
+            x1 = x2 = 1;
+            dx1 = aleatorio_decimal();
+            dx2 = aleatorio_decimal();
+            cuenta_carreras++;
+            hay_ganador = false;
+        }
+
+
+        _sleep(30);
+    }
+}
+
 void opcionesdemenu() {
     int escoger = 0;
-    mostrarcarga();
-    Sleep(200);
+    showIntro();
 
     system("cls");
 
@@ -61,6 +114,7 @@ void opcionesdemenu() {
 
         switch (escoger) {
         case 1:
+            IniciarCarrera();
             system("cls");
             break;
         case 2:
@@ -81,9 +135,11 @@ void opcionesdemenu() {
 
 int main()
 {
+    srand(time(nullptr));
 
+  
 
-    showIntro();
+    ConfigurarVentana();
     opcionesdemenu();
 
 
---
+++ GameJamEquisDe/MisFunciones.h
@@ -0,0 +1,75 @@
+#include "iostream"
+#include <Windows.h>
+using namespace System;
+using namespace std;
+
+const int HEIGHT = 200;
+const int WIDTH = 50;
+
+void ConfigurarVentana() {
+    Console::SetWindowSize(HEIGHT, WIDTH);
+    Console::CursorVisible = false;
+}
+
+void cursor(int x, int y) {
+    Console::SetCursorPosition(x, y);
+}
+
+void titulo(int x, int y) {
+    cursor(x, y);
+    cout << " CARRERA DE CARACTERES ";
+}
+
+void meta(int x, int y) {
+
+    for (int i = y; i < 40; i++) {
+        cursor(x, i); cout << "|";
+    }
+}
+
+float aleatorio_decimal() {
+    return  0.1 + ((float)(rand() % 60) / 80);
+}
+
+void panel_control(float vc1, float vc2, float x1, float x2, int cg1, int cg2) {
+
+    cursor(170, 10); cout << "Velocidad c1 " << vc1;
+    cursor(170, 11); cout << "Velocidad c2 " << vc2;
+
+    cursor(170, 13); cout << "pos X1: " << x1;
+    cursor(170, 14); cout << "pos X2: " << x2;
+
+    cursor(170, 16); cout << "Carreras ganadas C1: " << cg1;
+    cursor(170, 17); cout << "Carreras ganadas C2: " << cg2;
+
+
+}
+
+void dibuja_auto(int x, int y) {
+
+    cursor(x, y);     cout << "    __     ";
+    cursor(x, y + 1); cout << "  _|__|_   ";
+    cursor(x, y + 2); cout << " '-O---O-' ";
+}
+
+
+void borra_auto(int x, int y) {
+
+    cursor(x, y);     cout << "           ";
+    cursor(x, y + 1); cout << "           ";
+    cursor(x, y + 2); cout << "           ";
+}
+
+void dibujarauto2(int x, int y) {
+
+    cursor(x, y);     cout << "   __    ";
+    cursor(x, y + 1); cout << " _/  L_  ";
+    cursor(x, y + 2); cout << "'-O--O-' ";
+}
+
+void borraauto2(int x, int y) {
+
+    cursor(x, y);     cout << "           ";
+    cursor(x, y + 1); cout << "           ";
+    cursor(x, y + 2); cout << "           ";
+}
