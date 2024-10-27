ccfe4edd6225a77de48f81b6e57c1099a113a186
atahualpaxd <victortadeo207@gmail.com>
2024-10-26 22:04:58 -0500

    nuevo

--- GameJamEquisDe/GameJamEquisDe.cpp
+++ GameJamEquisDe/GameJamEquisDe.cpp
@@ -1,35 +1,92 @@
-#include "pch.h"
+#include "pch.h";
 #include <iostream>
+#include <Windows.h>
 using namespace System;
 using namespace std;
 
+const int HEIGHT = 120;
+const int WIDTH = 40;
 
-int numero(int a, int b) {
-    int n;
+void configurarventana() {
+    Console::SetWindowSize(HEIGHT, WIDTH);
+    Console::CursorVisible = false;
+}
+void mostrarcarga() {
 
-    cout << "ingresa dos numeros: "; cin >> n;
 
-    return a + b;
+    cout << "\n\n\n\n\n\n\t\t\t\t  ____  _                           _     _        \n";
+    cout << "\t\t\t\t | __ )(_) ___ _ ____   _____ _ __ (_) __| | ___   \n";
+    cout << "\t\t\t\t |  _ L| |/ _ L '_ Y L / / _ L '_ L| |/ _` |/ _ L  \n";
+    cout << "\t\t\t\t | |_) | |  __/ | | Y V /  __/ | | | | (_| | (_) | \n";
+    cout << "\t\t\t\t |____/|_|Y___|_| |_|Y_/ L___|_| |_|_|L__,_|L___/  ";
 
+
+
+
+    cout << "\n\n\n\n\n\n\n\n\n\n\t\t\t\t ";
+    char x = 219;
+    for (int i = 0; i <= 40; i++) {
+        Sleep(100);
+        cout << x;
+    }
 }
 
+int mostrarmenu() {
+    int elegir;
+
+
+    cout << "1. Jugar\n";
+    cout << "2. Creditos\n";
+    cout << "3. Salir\n\n";
+    cout << "Seleccione opcion: ";
+
+    do {
+        cin >> elegir;
+
+    } while (elegir < 1 || elegir > 3);
+    return elegir;
+}
+
+void opcionesdemenu() {
+    int escoger = 0;
+    mostrarcarga();
+    Sleep(200);
+
+    system("cls");
+
+    while (1) {
+        system("cls");
+        escoger = mostrarmenu();
+        system("cls");
+
+        switch (escoger) {
+        case 1:
+            system("cls");
+            break;
+        case 2:
+            cout << "Frank Flores\n\n";
+            cout << "Cielo Atencio\n\n";
+            cout << "Victor Hidago\n\n";
+
+            cout << "Presione cualquier tecla para continuar...\n\n";
+            system("pause > 0");
+            Console::ReadKey();
+            break;
+
+        case 3:
+            exit(0);
+        }
+    }
+}
 
 int main()
 {
-    int a, b;
-    int suma;
-
-    cout << "Ingrese primer numero: ";
-	cin >> a;
-
-	cout << "Ingrese segundo numero: ";
-	cin >> b;
 
 
+    configurarventana();
+    opcionesdemenu();
 
 
-
-    
-
+    system("pause>0");
     return 0;
 }
