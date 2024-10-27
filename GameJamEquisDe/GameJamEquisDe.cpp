092131ae4a3b331b140d77db70093d60a230a755
atahualpaxd <victortadeo207@gmail.com>
2024-10-26 21:56:58 -0500

    funcion suma

--- GameJamEquisDe/GameJamEquisDe.cpp
+++ GameJamEquisDe/GameJamEquisDe.cpp
@@ -1,8 +1,17 @@
 #include "pch.h"
 #include <iostream>
-
 using namespace System;
 using namespace std;
+
+int numero(int a, int b) {
+    int n;
+
+    cout << "ingresa dos numeros: "; cin >> n;
+
+    return a + b;
+
+}
+
 
 int main()
 {
